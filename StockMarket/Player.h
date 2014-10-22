14ba70e3c1e2aaaeb73a52091f4063ca06751f2b
ctrlthedark <ctrlthedark@gmail.com>
2014-10-21 21:41:04 -0400

    from cline

---
+++ StockMarket/Player.h
@@ -0,0 +1,18 @@
+//
+//  Player.h
+//  StockMarket
+//
+//  Created by Brandon Fink on 10/18/14.
+//  Copyright (c) 2014 Brandon Fink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Player : NSObject
+
+@property NSString *name;
+@property int money;
+@property NSMutableDictionary *portfolio;
+
+
+@end
---
+++ StockMarket/Player.m
@@ -0,0 +1,15 @@
+//
+//  Player.m
+//  StockMarket
+//
+//  Created by Brandon Fink on 10/18/14.
+//  Copyright (c) 2014 Brandon Fink. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+
+
+@end
